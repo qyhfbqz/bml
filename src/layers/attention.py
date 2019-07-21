0401175a3a38f43b440a6b840bd7ffad5faecfbf
Yan Feng <fengyan.jw@gmail.com>
2019-07-21 16:11:34 +0800

    update

--- src/layers/attention.py
+++ src/layers/attention.py
@@ -2,10 +2,34 @@ import tensorflow as tf
 from .base import Layer, Dense
 
 
+class ProductAttention(Layer):
+    def __init__(self, **kwargs):
+        super(ProductAttention, self).__init__(**kwargs)
+
+    def forward(self, query, value):
+        pass
+
+
+class ScaledProductAttention(Layer):
+    def __init__(self, **kwargs):
+        super(ScaledProductAttention, self).__init__(**kwargs)
+
+    def forward(self, query, value):
+        pass
+
+
+class BiLinearAttention(Layer):
+    def __init__(self, **kwargs):
+        super(BiLinearAttention, self).__init__(**kwargs)
+
+    def forward(self, query, value):
+        pass
+
+
 class MultiHeadAttention(Layer):
     def __init__(self, **kwargs):
 
         super(MultiHeadAttention, self).__init__(**kwargs)
 
-    def forward(self):
+    def forward(self, query, key, value):
         pass
