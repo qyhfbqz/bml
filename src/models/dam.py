373ab6a5e38887328119bb5b38e1105fdfb5857e
Yan Feng <fengyan.jw@gmail.com>
2019-07-22 14:14:11 +0800

    Create dam.py

---
+++ src/models/dam.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from layers.base import Layer
+
+
+class DeepAttentionMatch(Layer):
+    def __init__(self, **kwargs):
+        super(DeepAttentionMatch, self).__init__(**kwargs)
+
+    def forward(self):
+        pass
